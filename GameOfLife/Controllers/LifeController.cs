5652a4c80f7fd78b5a1d4b77d7234d34ef0c5516
tarnashi <tarnashi@yandex.ru>
2016-06-10 18:22:50 +0200

    first commit

---
+++ GameOfLife/Controllers/LifeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameOfLife.GameClasses;
+
+namespace GameOfLife.Controllers
+{
+    public class LifeController : Controller
+    {
+        private Field F; //подумать, зачем это надо?
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddField(int x, int y, bool f)
+        {
+            if (f)
+            {
+                F = new BorderedField(x, y);
+            }
+            else
+            {
+                F = new LoopbackField(x, y);
+            }
+            Session["F"] = F;
+            return RedirectToAction("Game", "Life");
+        }
+
+        public ActionResult Game()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ClearField()
+        {
+            var CurF = (Field)Session["F"];
+            CurF.Clear();
+            Session["F"] = CurF;
+            return this.Json(CurF, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetPlaner()
+        {
+            var CurF = (Field)Session["F"];
+            CurF.Clear();
+            CurF.ArrayCurrent[1, 0] = true;
+            CurF.ArrayCurrent[2, 1] = true;
+            CurF.ArrayCurrent[0, 2] = true;
+            CurF.ArrayCurrent[1, 2] = true;
+            CurF.ArrayCurrent[2, 2] = true;
+            Session["F"] = CurF;
+            return this.Json(CurF, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult ChangeCell(int x, int y)
+        {
+            var CurF = (Field) Session["F"];
+            CurF.ArrayCurrent[x, y] = !CurF.ArrayCurrent[x, y];
+            Session["F"] = CurF;
+            return this.Json(CurF, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetCurrentField()
+        {
+            var CurF = (Field) Session["F"];
+            return this.Json(CurF, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult MakeMove()
+        {
+            var CurF = (Field)Session["F"];
+            CurF.MakeMove();
+            Session["F"] = CurF;
+            return this.Json(CurF, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ GameOfLife/GameClasses/Field.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameOfLife.GameClasses
+{
+    public abstract class Field
+    {
+        //ПОЛЯ
+        //Размеры поля
+        public int width, height;
+        //Два массива: основной и буферный
+        public bool[,] ArrayCurrent;
+        //Режим поля: добавление, удаление, заблокировано
+        public byte mode;
+
+        public Field(int x, int y)
+        {
+            width = x;
+            height = y;
+            ArrayCurrent = new bool[x, y];
+            Clear();
+        }
+
+        public bool this[int x, int y]
+        {
+            get { return ArrayCurrent[x, y]; }
+        }
+
+        public void Clear()
+        {
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                {
+                    ArrayCurrent[x, y] = false;
+                }
+        }
+
+        public void MakeMove()
+        {
+            bool[,] ArrayTMP = new bool[width, height];
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                {
+                    byte localNubmerOfNeighbors = NumberOfNeighbors(x, y);
+                    if (ArrayCurrent[x, y])
+                    {
+                        if (localNubmerOfNeighbors < 2 || localNubmerOfNeighbors > 3)
+                        {
+                            //Если клетка жива и имеет меньше двух или больше трех живых соседей, она становится мертвой
+                            ArrayTMP[x, y] = false;
+                        }
+                        else
+                        {
+                            //Оставляем как есть
+                            ArrayTMP[x, y] = ArrayCurrent[x, y];
+                        }
+                    }
+                    else
+                    {
+                        if (localNubmerOfNeighbors == 3)
+                        {
+                            //Если клетка мертва и имеет трех живых соседей, она становится живой
+                            ArrayTMP[x, y] = true;
+                        }
+                        else
+                        {
+                            //Оставляем как есть
+                            ArrayTMP[x, y] = ArrayCurrent[x, y];
+                        }
+                    }
+                }
+
+            //Просто меняем ссылку на массив. Не пахнет ли такой вариант?
+            ArrayCurrent = ArrayTMP;
+
+        }
+
+        public abstract byte NumberOfNeighbors(int x, int y);
+
+    }
+}
